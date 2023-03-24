20656068dd5b87f2208a8544c88412ca15719691
Hoamp <setiawanthomas43@gmail.com>
2023-03-24 11:14:58 +0700

    simple auth api

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -3,9 +3,94 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
-    //
+    public function register(Request $request)
+    {
+        // validasi inputan user
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|confirmed'
+        ]);
+
+        // jika ada yang salah
+        if ($validator->fails()) {
+            // kembalikan response gagal
+            return response()->json([
+                'success' => false,
+                'message' => 'Gagal register',
+                'data' => $validator->errors()
+            ], 400);
+        }
+
+        // enkripsi password
+        $input = $request->all();
+        $input['password'] = bcrypt($input['password']);
+
+        // buat user baru
+        $user = User::create($input);
+
+        // buat token
+        $success['token'] = $user->createToken('auth_token')->plainTextToken;
+        $success['name'] = $user->name;
+
+        // kembalikan response
+        return response()->json([
+            'success' => true,
+            'message' => 'Registrasi sukses',
+            'data' => $success
+        ], 201);
+    }
+
+    public function login(Request $request)
+    {
+        // mencocokkan email dan password
+        $credentials = [
+            'email' => $request->email,
+            'password' => $request->password
+        ];
+
+        // jika ada user yang cocok
+        if (Auth::attempt($credentials)) {
+            // buat token baru
+            $auth = Auth::user();
+            $success['token'] = $auth->createToken('auth_token')->plainTextToken;
+            $success['name'] = $auth->name;
+            $success['email'] = $auth->email;
+
+
+            // kembalikan response
+            return response()->json([
+                'success' => true,
+                'message' => 'Login sukses',
+                'data' => $success
+            ], 200);
+        } else {
+            // jika tidak ada user kembalikan response
+            return response()->json([
+                'success' => false,
+                'message' => 'Cek email dan password lagi',
+                'data' => null
+            ], 400);
+        }
+    }
+
+
+    public function logout(Request $request)
+    {
+        // hapus token
+        auth()->user()->tokens()->delete();
+
+        // kembalikan response
+        return response()->json([
+            'success' => true,
+            'message' => 'Berhasil logout'
+        ]);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// public api
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+
+// private api
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    // logout user
+    Route::post('/logout', [AuthController::class, 'logout']);
 });
